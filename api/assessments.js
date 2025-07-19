92eb3841269ab274beb2865a6b93b5962115c147
stella-wyj <s.wangyj06@gmail.com>
2025-07-19 14:05:44 -0400

    trying to incorporate vercel

--- api/assessments.js
+++ api/assessments.js
@@ -1,11 +1,10 @@
 export default function handler(req, res) {
-    // Example: return a list of assessments
-    if (req.method === 'GET') {
-      res.status(200).json([
-        { id: 1, courseId: 1, name: "Quiz 1", grade: 90 },
-        { id: 2, courseId: 2, name: "Lab 1", grade: 85 }
-      ]);
-    } else {
-      res.status(405).json({ error: "Method not allowed" });
-    }
-  } 
+  if (req.method === 'GET') {
+    res.status(200).json([
+      { id: 1, name: "Math" },
+      { id: 2, name: "Science" }
+    ]);
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
